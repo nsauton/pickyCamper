bf87da0612125299b03ced1f84627374f9a55902
Nathan Sauton <nsauton@ucsc.edu>
2025-12-17 15:16:14 -0800

    added db and adding recipe route

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
-from typing import Annotated
+from typing import List, Annotated
 import models
 from database import engine, SessionLocale
 from sqlalchemy.orm import Session
@@ -9,6 +9,27 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 # write basemodels here
+class IngredientBase(BaseModel):
+    name: str
+
+class MeasurementBase(BaseModel):
+    ingredient: IngredientBase
+    quantity: float
+    unit: str
+
+class RecipeBase(BaseModel):
+    name: str
+    meal: str
+    instructions: str
+    source: str
+    category: str
+    servingSize: float
+    time: int
+    effortRating: int
+    picture: str
+    dehydrate: bool
+    measurements: List[MeasurementBase]
+
 
 def get_db():
     db = SessionLocale()
@@ -23,3 +44,47 @@ db_dependecy = Annotated[Session, Depends(get_db)]
 @app.get("/")
 async def root():
     return {"message": "Hello world! this is the pickyCamper API"}
+
+@app.post("/recipes")
+async def add_recipe(recipe: RecipeBase, db: db_dependecy):
+    #add the recipe
+    db_recipe = models.Recipes(name = recipe.name, 
+                               meal = recipe.meal,
+                               instructions = recipe.instructions,
+                               source = recipe.source,
+                               category = recipe.category, 
+                               servingSize = recipe.servingSize, 
+                               time = recipe.time,
+                               effortRating = recipe.effortRating, 
+                               picture = recipe.picture, 
+                               dehydrate = recipe.dehydrate)
+    db.add(db_recipe)
+    db.commit()
+    db.refresh(db_recipe)
+
+    for measurement in recipe.measurements:
+        #check if ingredient already exists, if not add it
+        db_ingredient = db.query(models.Ingredients).filter(models.Ingredients.name == measurement.ingredient.name).first()
+        if not db_ingredient:
+            db_ingredient = models.Ingredients(name = measurement.ingredient.name)
+            db.add(db_ingredient)
+            db.commit()
+            db.refresh(db_ingredient)
+            
+        #add the measurements
+        db_measurement = models.Measurements(quantity = measurement.quantity,
+                                             unit = measurement.unit,
+                                             recipeID = db_recipe.id, 
+                                             ingredientID = db_ingredient.id)
+        db.add(db_measurement)
+
+    db.commit()
+
+    return {"recipeID": db_recipe.id}
+        
+@app.get("/recipes/{recipeID}")
+async def get_recipe(recipeID: int, db: db_dependecy):
+    res = db.query(models.Recipes).filter(models.Recipes.id == recipeID).first()
+    if not res:
+        raise HTTPException(status_code=404, detail='Recipe not found')
+    return res
--- backend/models.py
+++ backend/models.py
@@ -1,2 +1,32 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Boolean
 from database import Base
+
+class Recipes(Base):
+    __tablename__ = 'recipes'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, default='')
+    meal = Column(String, default='')
+    instructions = Column(String, default='')
+    source = Column(String, default='')
+    category = Column(String, default='')
+    servingSize = Column(Float)
+    time = Column(Integer)
+    effortRating = Column(Integer)
+    picture = Column(String, default='') #url or filename
+    dehydrate = Column(Boolean)
+
+class Ingredients(Base):
+    __tablename__ = 'ingredients'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, default='')
+
+class Measurements(Base):
+    __tablename__ = 'measurements'
+
+    id = Column(Integer, primary_key=True, index=True)
+    recipeID = Column(Integer, ForeignKey("recipes.id"))
+    ingredientID = Column(Integer, ForeignKey("ingredients.id"))
+    quantity = Column(Float, default=0.0)
+    unit = Column(String, default='')
